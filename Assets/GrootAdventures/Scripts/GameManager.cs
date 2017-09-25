3c108231b3f8ffe19be0d4fdb65400e67bb91904
jAbecassis <judicael.abecassis@gmail.com>
2017-09-25 05:41:08 +0200

    Buttons

---
+++ Assets/GrootAdventures/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    void StartLevel()
+    {
+
+    }
+
+    void WinLevel()
+    {
+
+    }
+
+    void LooseLevel()
+    {
+
+    }
+}
